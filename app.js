1b2cb143a03d7926499d06854cf9b11f1373537a
unknown <vip_heromen_vip@mail.ru>
2019-12-07 01:57:14 +0500

    Finishing the first part of Project plan

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var budgetController = (function() {
 		this.value = value;
 	};
 
+	var calculateTotal = function(type) {
+		var sum = 0;
+		data.allItems[type].forEach(function (cur) {
+			sum += cur.value;
+		});
+		data.totals[type] = sum;
+	};
+
 // the data structure where we will store all expencies and incomes objects;
 	var data = {
 		allItems: {
@@ -24,7 +32,10 @@ var budgetController = (function() {
 		totals: {
 			exp: 0,
 			inc: 0
-		}
+		},
+
+		budget: 0,
+		percentage: -1
 	};
 
 	return {
@@ -56,6 +67,34 @@ var budgetController = (function() {
 			return newItem;
 		},
 
+		calculateBudget: function() {
+
+			// calculate total income and expenses
+			calculateTotal('exp');
+			calculateTotal('inc');
+
+			// Calculate the budget: income-expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+			// calculate the percentage of income that we spent
+			if(data.totals.inc > 0) {
+				data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+			} else {
+				data.percentage = -1;
+			}
+			
+			// Expence = 100 and income 200, spent 50% = 100 / 200 = 0.5*100
+		},
+
+		getBudget: function() {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage
+			}
+		},
+
 		testing: function() {
 			console.log(data);
 		}
@@ -76,6 +115,10 @@ var UIController = (function() {
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
 		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	}
 
 //Public function which can use private functions
@@ -85,7 +128,7 @@ var UIController = (function() {
 			return {
 				 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
 				 description: document.querySelector(DOMstrings.inputDescription).value,
-				 value: document.querySelector(DOMstrings.inputValue).value
+				 value: parseFloat(document.querySelector(DOMstrings.inputValue).value)// Will contain not a string but a number in the value vield
 			}
 		},
 
@@ -98,7 +141,6 @@ var UIController = (function() {
 				html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			} else if(type === 'exp'){
 				element = DOMstrings.expensesContainer;
-				console.log(element);
 				html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
 			}
 
@@ -109,6 +151,38 @@ var UIController = (function() {
 
 			// Insert the HTMl into the DOM
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+		},
+
+		clearFields: function() {			
+			var fields, fieldsArr;
+
+			// Fields variable will have a list inside
+			fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+			// We take from Array prototype method slice to convert the list(in this case) into an array;
+			fieldsArr = Array.prototype.slice.call(fields);
+
+			// Here we have access to the current element, index of the current element and entire array.
+			fieldsArr.forEach(function(current, index, array) {
+				current.value = "";
+			});
+
+			// Here we put the focus on the webpage to the add__description field.
+			fieldsArr[0].focus();
+		},
+
+		displayBudget: function(obj) {
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			
+
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else{
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
 
 		},
 
@@ -119,7 +193,6 @@ var UIController = (function() {
 
 })();
 
-//Here  we pass our earlier created objects as arguments with different name to make controller module more independent
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
@@ -138,6 +211,17 @@ var controller = (function(budgetCtrl, UICtrl) {
 		});
 	};
 
+	var updateBudget = function() {
+		// 1. Calculate the budget
+		budgetCtrl.calculateBudget();
+
+		// 2. Return the budget
+		var budget = budgetCtrl.getBudget();
+
+		// 3. Display the budget on the UI
+		UICtrl.displayBudget(budget);
+
+	}
 
 	var ctrlAddItem = function() {
 		var input, newItem;
@@ -145,26 +229,35 @@ var controller = (function(budgetCtrl, UICtrl) {
 		// 1. Get the field input data
 		input = UICtrl.getInput();
 
-		// 2. Add the item to the budget controller
-		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+		if (input.description !== "" && !isNaN(input.value) && input.value > 0){
+			// 2. Add the item to the budget controller
+			newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-		// 3. Add the item to the UI
-		UICtrl.addListItem(newItem, input.type);
+			// 3. Add the item to the UI
+			UICtrl.addListItem(newItem, input.type);
 
-		// 4. Calculate the budget
+			// 4. Clear the fields
+			UICtrl.clearFields();
 
-		// 5. Display the budget on the UI
-
+			// 5 Calculate and update budget
+			updateBudget();
+		}
 	};
 
 	return {
 		init: function() {
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			console.log('Application has started.');
 			setupEventListeners();
 		}
 	}
 
-})(budgetController, UIController);
+})(budgetController, UIController); //Here  we pass our earlier created objects as arguments with different name to make controller module more independent
 
 controller.init();
 
